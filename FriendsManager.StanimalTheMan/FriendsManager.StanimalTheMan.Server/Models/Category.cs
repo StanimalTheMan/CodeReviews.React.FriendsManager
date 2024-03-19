6beaf0fd8c7af4f8ad883a50a59ad72186da77ec
Stan Choi <sjc591@nyu.edu>
2024-03-19 17:11:12 -0400

    Initialize project and create Friend, Category models

---
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FriendsManager.StanimalTheMan.Server.Models;
+
+public class Category
+{
+    public int CategoryID { get; set; }
+
+    [Required(ErrorMessage = "Category name is required")]
+    public string Name { get; set; }
+
+    public virtual ICollection<Friend> Friends { get; set; }
+}
---
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Models/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FriendsManager.StanimalTheMan.Server.Models;
+
+public class Friend
+{
+    public int FriendID { get; set; }
+    public string Name { get; set; }
+    public DateTime LastContactDate { get; set; }
+    public string LastContactType { get; set; } // don't want to use enums for now as user can be flexible, e.g. phone call, in person, facetime
+    public string DesiredContactFrequency { get; set; } // daily, weekly, monthly, annually
+
+    public int CategoryID { get; set; }
+
+    [ForeignKey("CategoryID")]
+    public Category Category { get; set; }
+}
