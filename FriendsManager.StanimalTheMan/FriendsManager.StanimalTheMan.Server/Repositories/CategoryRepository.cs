bf317fa01afc9b03341d53e8eb045b5ac7e5e399
Stan Choi <sjc591@nyu.edu>
2024-03-19 19:12:43 -0400

    Create service folder and implement Category repository

---
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Repositories/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using FriendsManager.StanimalTheMan.Server.Data;
+using FriendsManager.StanimalTheMan.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendsManager.StanimalTheMan.Server.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly FriendsManagerDbContext _context;
+
+    public CategoryRepository(FriendsManagerDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddCategoryAsync(Category category)
+    {
+        await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCategoryAndFriendsAsync(Category category)
+    {
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                _context.Friends.RemoveRange(category.Friends);
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw new Exception("Error deleting category and friends.", ex);
+            }
+        }
+    }
+
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category> GetCategoryByIdAsync(int id)
+    {
+        return await _context.Categories.FindAsync(id);
+    }
+
+    public async Task UpdateCategoryAsync(Category category)
+    {
+        _context.Entry(category).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using FriendsManager.StanimalTheMan.Server.Models;
+
+namespace FriendsManager.StanimalTheMan.Server.Repositories;
+
+public interface ICategoryRepository
+{
+    Task AddCategoryAsync(Category category);
+    Task<Category> GetCategoryByIdAsync(int id);
+    Task<IEnumerable<Category>> GetAllCategoriesAsync();
+    Task UpdateCategoryAsync(Category category);
+    Task DeleteCategoryAndFriendsAsync(Category category);
+}
