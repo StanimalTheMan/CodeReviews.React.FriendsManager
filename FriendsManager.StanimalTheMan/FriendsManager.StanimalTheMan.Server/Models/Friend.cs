f0938095daa93c7b50bafc9cd2e71ee6947ef4c1
Stan Choi <sjc591@nyu.edu>
2024-03-20 19:30:26 -0400

    Add annotations

--- FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Models/Friend.cs
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Models/Friend.cs
@@ -1,13 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FriendsManager.StanimalTheMan.Server.Models;
 
 public class Friend
 {
+    [Key]
     public int FriendID { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "Last Contact Date is required")]
+    [Display(Name = "Last Contact Date")]
+    [DataType(DataType.Date)]
     public DateTime LastContactDate { get; set; }
+
+    [Required(ErrorMessage = "Last Contact Type is required")]
+    [Display(Name = "Last Contact Type")]
     public string LastContactType { get; set; } // don't want to use enums for now as user can be flexible, e.g. phone call, in person, facetime
+
+    [Required(ErrorMessage = "Desired Contact Frequency is required")]
+    [Display(Name = "Desired Contact Frequency")]
     public string DesiredContactFrequency { get; set; } // daily, weekly, monthly, annually
 
     public int CategoryID { get; set; }
