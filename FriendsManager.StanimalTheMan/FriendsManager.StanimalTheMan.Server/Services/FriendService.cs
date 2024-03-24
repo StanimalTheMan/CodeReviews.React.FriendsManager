2f0f7460b556c67eb8a4f96e90824e9a855459f9
Stan Choi <sjc591@nyu.edu>
2024-03-24 10:21:04 -0400

    Add Friend Service Layer

---
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Services/FriendService.cs
@@ -0,0 +1,41 @@
+﻿using FriendsManager.StanimalTheMan.Server.Models;
+using FriendsManager.StanimalTheMan.Server.Repositories;
+
+namespace FriendsManager.StanimalTheMan.Server.Services;
+
+public class FriendService : IFriendService
+{
+    private readonly IFriendRepository _friendRepository;
+
+    public FriendService(IFriendRepository friendRepository)
+    {
+        _friendRepository = friendRepository;
+    }
+
+    public async Task AddFriendAsync(Friend friend)
+    {
+        await _friendRepository.AddFriendAsync(friend);
+    }
+
+    public async Task DeleteFriendAsync(Friend friend)
+    {
+        await _friendRepository.DeleteFriendAsync(friend);
+    }
+
+    public async Task<IEnumerable<Friend>> GetAllFriendsAsync()
+    {
+        return await _friendRepository.GetAllFriendsAsync();
+    }
+
+    public async Task<Friend> GetFriendByIdAsync(int id)
+    {
+        return await _friendRepository.GetFriendByIdAsync(id);
+    }
+
+    public async Task UpdateFriendAsync(Friend friend)
+    {
+        await _friendRepository.UpdateFriendAsync(friend);
+    }
+}
+
+
---
+++ FriendsManager.StanimalTheMan/FriendsManager.StanimalTheMan.Server/Services/IFriendService.cs
@@ -0,0 +1,13 @@
+﻿using FriendsManager.StanimalTheMan.Server.Models;
+
+namespace FriendsManager.StanimalTheMan.Server.Services;
+
+public interface IFriendService
+{
+    Task AddFriendAsync(Friend friend);
+
+    Task DeleteFriendAsync(Friend friend);
+    Task<IEnumerable<Friend>> GetAllFriendsAsync();
+    Task<Friend> GetFriendByIdAsync(int id);
+    Task UpdateFriendAsync(Friend friend);
+}
